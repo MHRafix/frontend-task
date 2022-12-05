f9fe0c9c73af50d920da723a10b08ee8568fd5d2
MHRafix <fmhrafiz27@gmail.com>
2022-12-05 21:04:00 +0600

    error fixed:-

--- src/pages/allProducts.tsx
+++ src/pages/allProducts.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@mui/material";
 import Box from "@mui/material/Box";
 import Cookies from "js-cookie";
 import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import Router from "next/router";
 import { useEffect } from "react";
+import { AiFillEdit } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
 import LayoutContainer from "../components/common/Layout/LayoutContainer";
 import db from "../hooks/db";
 import Product from "../model/Product";
@@ -82,108 +87,110 @@ const AllProducts: NextPage<{ allProducts: IProduct[] | any[] }> = ({
             Actions
           </Box>
         </Box>
-        {/* {allProducts?.length ? (
-					<Box>
-						{allProducts?.map((product: IProduct, idx: number) => (
-							<Box
-								key={idx}
-								sx={{
-									width: '700px',
-									margin: '15px auto',
-									background: '#f1f1f1',
-									padding: '5px',
-									display: 'flex',
-									alignItems: 'center',
-									textAlign: 'center',
-									borderRadius: '10px',
-									cursor: 'pointer',
-								}}
-							>
-								<Box
-									sx={{
-										width: '20%',
-										textAlign: 'center',
-									}}
-								>
-									{product._id?.slice(0, 5)}
-								</Box>
-								<Box
-									sx={{
-										width: '20%',
-										textAlign: 'center',
-									}}
-								>
-									{product.title}
-								</Box>
-								<Box
-									sx={{
-										width: '20%',
-										textAlign: 'center',
-									}}
-								>
-									${product.regular_price}
-								</Box>
-								<Box
-									sx={{
-										display: 'flex',
-										justifyContent: 'center',
-										alignItems: 'center',
-										width: '20%',
-										textAlign: 'center',
-									}}
-								>
-									{product.thumbnail && (
-										<Image
-											src={product?.thumbnail}
-											alt='thumbnail'
-											width={50}
-											height={50}
-											style={{
-												borderRadius: '100px',
-											}}
-										/>
-									)}
-								</Box>
-								<Box>
-									<Button
-										variant='contained'
-										color='error'
-										// onClick={() =>
-										// 	handleDelete(
-										// 		products,
-										// 		setProducts,
-										// 		product._id,
-										// 		`product/delete/${product._id}`
-										// 	)
-										// }
-									>
-										<MdDelete size={20} />
-									</Button>
-									&nbsp;
-									<Link href={`/editProduct/${product._id}`} passHref>
-										<Button variant='contained' color='secondary'>
-											<AiFillEdit size={20} />
-										</Button>
-									</Link>
-								</Box>
-							</Box>
-						))}
-					</Box>
-				) : (
-					<Box
-						sx={{
-							width: '700px',
-							margin: '15px auto',
-							padding: '5px',
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							color: 'red',
-						}}
-					>
-						No products found!
-					</Box>
-				)} */}
+        <>
+          {allProducts?.length ? (
+            <Box>
+              {allProducts?.map((product: IProduct, idx: number) => (
+                <Box
+                  key={idx}
+                  sx={{
+                    width: "700px",
+                    margin: "15px auto",
+                    background: "#f1f1f1",
+                    padding: "5px",
+                    display: "flex",
+                    alignItems: "center",
+                    textAlign: "center",
+                    borderRadius: "10px",
+                    cursor: "pointer",
+                  }}
+                >
+                  <Box
+                    sx={{
+                      width: "20%",
+                      textAlign: "center",
+                    }}
+                  >
+                    {product._id?.slice(0, 5)}
+                  </Box>
+                  <Box
+                    sx={{
+                      width: "20%",
+                      textAlign: "center",
+                    }}
+                  >
+                    {product.title}
+                  </Box>
+                  <Box
+                    sx={{
+                      width: "20%",
+                      textAlign: "center",
+                    }}
+                  >
+                    ${product.regular_price}
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      width: "20%",
+                      textAlign: "center",
+                    }}
+                  >
+                    {product.thumbnail && (
+                      <Image
+                        src={product?.thumbnail}
+                        alt="thumbnail"
+                        width={50}
+                        height={50}
+                        style={{
+                          borderRadius: "100px",
+                        }}
+                      />
+                    )}
+                  </Box>
+                  <Box>
+                    <Button
+                      variant="contained"
+                      color="error"
+                      // onClick={() =>
+                      // 	handleDelete(
+                      // 		products,
+                      // 		setProducts,
+                      // 		product._id,
+                      // 		`product/delete/${product._id}`
+                      // 	)
+                      // }
+                    >
+                      <MdDelete size={20} />
+                    </Button>
+                    &nbsp;
+                    <Link href={`/editProduct/${product._id}`} passHref>
+                      <Button variant="contained" color="secondary">
+                        <AiFillEdit size={20} />
+                      </Button>
+                    </Link>
+                  </Box>
+                </Box>
+              ))}
+            </Box>
+          ) : (
+            <Box
+              sx={{
+                width: "700px",
+                margin: "15px auto",
+                padding: "5px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                color: "red",
+              }}
+            >
+              No products found!
+            </Box>
+          )}
+        </>
       </Box>
     </LayoutContainer>
   );
